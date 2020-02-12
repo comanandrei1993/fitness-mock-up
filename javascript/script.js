298df21407ac12de95e383a55c12234394ea3830
comanandrei1993 <comanandrei1993@gmail.com>
2020-02-12 08:25:47 +0200

    - nav styles
    Added a function that changes the color of the link depending on the section the user is hovering over

--- javascript/script.js
+++ javascript/script.js
@@ -5,7 +5,7 @@ const returnPageSections = () => {
 
     const pageSections = [];
 
-    for(i = 0; i <= sections.length - 1; i++) {
+    for(let i = 0; i <= sections.length - 1; i++) {
         let selector = document.querySelector(`${sections[i]}`);
         pageSections.push(selector);
     }
@@ -19,7 +19,7 @@ const returnNavElements = () => {
 
     const navElements = [];
 
-    for(i = 0; i <= elements.length - 1; i++) {
+    for(let i = 0; i <= elements.length - 1; i++) {
         let selector = document.querySelector(`${elements[i]}`);
         navElements.push(selector);
     }
@@ -31,20 +31,36 @@ const returnNavElements = () => {
 const makeNavObjects = () => {
     const navElements = returnNavElements();
     const pageSections = returnPageSections();
+    const sectionYPositons = returnSectionYPositions();
 
     let navObjects = [];
 
-    for(i = 0; i <= navElements.length - 1; i++) {
+    for(let i = 0; i <= navElements.length - 1; i++) {
         navObjects.push({
             navElement: navElements[i],
-            pageSection: pageSections[i]
+            pageSection: pageSections[i],
+            pageSectionYPosition: sectionYPositons[i]
         })
     }
 
     return navObjects;
 }
 
-// add event listeners
+// get clientHeight (y position) of section elements
+const returnSectionYPositions = () => {
+    let pageSections = returnPageSections();
+
+    const sectionYPositons = [];
+
+    pageSections.forEach(el => {
+        sectionYPositons.push(el.getBoundingClientRect().top);
+    });
+
+    return sectionYPositons;
+}
+
+// add navbar event listeners
+// change nav element color on navlink click
 const navEventListener = () => {
     const navObjects = makeNavObjects();
 
@@ -53,7 +69,7 @@ const navEventListener = () => {
     navObjects.forEach(el => {
         
         el.navElement.addEventListener('click', function() {
-            for (i = 0; i <= navElements.length - 1; i++) {
+            for (let i = 0; i <= navElements.length - 1; i++) {
                 navElements[i].style.color = '#6c6e8f';
             }
             this.style.color = '#54BF9D';
@@ -61,3 +77,21 @@ const navEventListener = () => {
     })
 }
 navEventListener();
+
+// mouse over section event listeners 
+// change nav element color on hover over page section
+const mouseOverSection = () => {
+    const navObjects = makeNavObjects();
+
+    navObjects.forEach(el => {
+        console.log(el.pageSection);
+        el.pageSection.addEventListener('mouseover', function() {
+            console.log(el.pageSection);
+            for (let i = 0; i <= navObjects.length - 1; i++) {
+                navObjects[i].navElement.style.color = '#6c6e8f';
+            }
+            el.navElement.style.color = '#54BF9D';
+        })
+    })
+}
+mouseOverSection();
